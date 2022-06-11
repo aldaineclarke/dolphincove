5d722e4fbd5459bab5d07953454e601e300056a5
aldaine-AG <aldaineclarke1@gmail.com>
2022-06-11 13:32:54 -0500

    fixed the connection to the database

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 const db = mysql.createConnection({
-    host: process.env.SECRET_KEY,
+    host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     user: process.env.DB_USER,
