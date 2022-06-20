ed97538aada02f95d4410c45f80f822d28e3e09e
aldaine-AG <aldaineclarke1@gmail.com>
2022-06-19 22:39:18 -0500

    Added middlewares for authentication

---
+++ middleware/auth.guard.js
@@ -0,0 +1,16 @@
+function authenticated(req, res,next){
+    if(!req.session.user){
+        return res.redirect('/auth/login');
+    }
+    next();
+}
+function isSuperAdmin(req, res, next){
+    if(req.session.user.company_id != 1000){
+        return res.redirect('/auth/bookings');
+    }
+    next();
+}
+module.exports={
+    authenticated,
+    isSuperAdmin
+}
